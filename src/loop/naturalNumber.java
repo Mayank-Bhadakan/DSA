30a3329cb32e663c1288b16cd101b9463ff80a71
Mayank Bhadakan <mayankbhadakan95@gmail.com>
2026-01-10 19:55:40 -0800

    print natural number upto n

---
+++ src/loop/naturalNumber.java
@@ -0,0 +1,16 @@
+package loop;
+
+import java.util.Scanner;
+
+public class naturalNumber {
+    public static void main(String[] args){
+        Scanner sc  = new Scanner (System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        for (int i=0; i<=n; i++){
+            System.out.print(i  + " ");
+        }
+    }
+}
