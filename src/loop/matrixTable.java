1b92001aa07599b3b582608033348dfa5ce10b77
Mayank Bhadakan <mayankbhadakan95@gmail.com>
2026-01-10 20:05:17 -0800

    matrix table

---
+++ src/loop/matrixTable.java
@@ -0,0 +1,16 @@
+package loop;
+
+import java.util.Scanner;
+
+public class matrixTable {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter number which table you want: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=10; i++){
+            System.out.println( n + " * " + i + " = " + (n*i));
+        }
+    }
+}
